cf1fe6e4be3ed1eb37c3a92bfc8eca78dcbf0cfe
Rael Souza da Paixão <rsouza2123@gmail.com>
2025-09-11 12:03:59 -0300

    Conexão do banco de Viagens - Feito

--- FleetOn/Repositories/ViagemRepository.cs
+++ FleetOn/Repositories/ViagemRepository.cs
@@ -1,7 +1,10 @@
 ﻿using FleetOn.Interfaces;
 using FleetOn.Models;
+using FleetOn.Services;
+using Npgsql;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,35 +14,8 @@ namespace FleetOn.Repositories
     public class ViagemRepository:IViagemRepository
     {
         private readonly List<Viagem> _Viagem;
-        private readonly List<Cliente> _Clientes;
-        private readonly List<Motorista> _motoristas;
-        
-        public ViagemRepository()
-        {
-            // Simulação de dados (poderia vir de banco)
-            _motoristas = new List<Motorista>
-            {
-                new Motorista { Id = 1, Nome = "Carlos Silva", Cnh = "123456789", Ativo = true },
-                new Motorista { Id = 2, Nome = "Maria Souza", Cnh = "987654321", Ativo = false },
-                new Motorista { Id = 3, Nome = "João Pereira", Cnh = "555444333", Ativo = true }
-            };
-            // Simulação de dados (poderia vir de banco)
-            _Clientes = new List<Cliente>
-            {
-                new Cliente { Id = 1, Nome = "Carlos Silva", Doc = "123456789", Ativo = true },
-                new Cliente { Id = 2, Nome = "Maria Souza", Doc = "987654321", Ativo = false },
-                new Cliente { Id = 3, Nome = "João Pereira", Doc = "555444333", Ativo = true }
-            };
-
-            // Simulação de dados (poderia vir de banco)
-            _Viagem = new List<Viagem>
-            {
-                new Viagem { Id = 1, cliente = _Clientes[0], motorista = _motoristas[0], date = DateTime.Now,localCarga = "Rua 1", Ativo = true },
-                new Viagem { Id = 2, cliente = _Clientes[1], motorista = _motoristas[1], date = DateTime.Now,localCarga = "Rua B", Ativo = false },
-                new Viagem { Id = 3, cliente = _Clientes[2], motorista = _motoristas[2], date = DateTime.Now,localCarga = "Rua C", Ativo = true }
-            };
-        }
-
+        private readonly ClienteService _Clientes;
+        private readonly MotoristaService _Motorista;
         public Viagem GetById(int id)
         {
             return _Viagem.FirstOrDefault(m => m.Id == id);
@@ -47,17 +23,54 @@ namespace FleetOn.Repositories
 
         public IEnumerable<Viagem> GetAll()
         {
-            return _Viagem;
+            var lista = new List<Viagem>();
+
+            string sql = "SELECT id_viagem, id_cliente, id_motorista FROM Viagens";
+
+            var dt = PostgresHelper.GetDataTable(sql);
+
+            foreach (DataRow row in dt.Rows)
+            {
+                lista.Add(new Viagem
+                {
+                    Id = (int)row["id_Viagem"],
+                    cliente = _Clientes.BuscarPorId((int)row["id_cliente"]),
+                    motorista = _Motorista.BuscarPorId((int)row["id_motorista"])
+                });
+            }
+
+            return lista;
         }
 
         public IEnumerable<Viagem> GetAtivos()
         {
-            return _Viagem.Where(m => m.Ativo);
+            var lista = new List<Viagem>();
+
+            string sql = "SELECT id_viagem, id_cliente, id_motorista FROM Viagens";
+
+            var dt = PostgresHelper.GetDataTable(sql);
+
+            foreach (DataRow row in dt.Rows)
+            {
+                lista.Add(new Viagem
+                {
+                    Id = (int)row["id_Viagem"],
+                    cliente = _Clientes.BuscarPorId((int)row["id_cliente"]),
+                    motorista = _Motorista.BuscarPorId((int)row["id_motorista"])
+                });
+            }
+
+            return lista;
         }
 
         public void PostViagem(Viagem m)
         {
-            _Viagem.Add(m);
+            string sql = "INSERT INTO viagens (id_cliente, id_motorista) VALUES (@id_cliente, @id_motorista)";
+
+            PostgresHelper.ExecuteScalar(sql,
+                new NpgsqlParameter("@id_cliente", m.cliente.Id),
+                new NpgsqlParameter("@id_motorista", m.motorista.Id)
+            );
         }
     }
 }
